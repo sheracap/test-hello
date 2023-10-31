75bf1b51c039365626a317aa4c127844aa7a9560
sherzod <sherzodtursunov1996@gmail.com>
2023-10-31 18:51:04 +0500

    for test database changes

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,6 +1,62 @@
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export default function Dashboard() {
+
+  const [email, setEmail] = useState("");
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  const getUsers = async () => {
+    const res = await fetch("http://localhost:5000/api/testusers");
+
+    return res.json().then((val) => {
+      setUsers(val);
+    });
+  };
+
+  const onCreateUser = async () => {
+    const data = {
+      email
+    };
+
+    const res = await fetch("http://localhost:5000/api/testusers", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data)
+    });
+
+    return res.json().then(() => {
+      alert("Создан");
+      setEmail("");
+      getUsers();
+    });
+  };
+
+  const onDelete = async (id: any) => {
+    const data = {
+      id
+    };
+
+    const res = await fetch("http://localhost:5000/api/testusers", {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data)
+    });
+
+    return res.json().then(() => {
+      alert("Удален");
+      setEmail("");
+      getUsers();
+    });
+  };
 
   return (
     <div>
@@ -8,6 +64,23 @@ export default function Dashboard() {
       <Link to="/auth">
         1234
       </Link>
+      <input
+        type="text"
+        value={email}
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+      />
+      <div onClick={() => onCreateUser()}>
+        Create
+      </div>
+      <div style={{ paddingTop: "20px" }}>
+        {users.map((item: any, index) => (
+          <div key={item.id}>
+            {index + 1} {item.email} --------------- <span onClick={() => onDelete(item.id)}>Удалить</span>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
